476d62a7da5416dc837bbbe880d86abb7a9c1260
Brianosumba <144438563+Brianosumba@users.noreply.github.com>
2024-09-17 18:04:56 +0200

    Homepage complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HomePage from "./pages/Homepage/HomePage";
+import Footer from "./components/Footer"; // Import Footer component
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="flex flex-col min-h-screen">
       <HomePage />
+      <Footer />
     </div>
   );
 };
