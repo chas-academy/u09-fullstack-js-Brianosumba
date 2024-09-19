37e3aed0531cb5a5f246d009b94164e548fc35e9
Brianosumba <144438563+Brianosumba@users.noreply.github.com>
2024-09-19 14:19:43 +0200

    fix

--- frontend/src/components/ExerciseCards.jsx
+++ frontend/src/components/ExerciseCards.jsx
@@ -2,7 +2,7 @@ import { GiWeightLiftingUp } from "react-icons/gi";
 
 const ExerciseCards = ({ title }) => {
   return (
-    <div className="bg-slate-300 border border-gray-400 rounded-lg shadow-md p-4 flex flex-col items-center hover:shadow-lg trasition duration-300 hover:scale-105">
+    <div className="bg-gray-400 border border-gray-300 rounded-lg shadow-md p-4 flex flex-col items-center hover:shadow-lg trasition duration-300 hover:scale-105">
       <div className="flex items-center justify-center mb-2">
         <GiWeightLiftingUp className="text-4xl text-gray-700" />
       </div>
