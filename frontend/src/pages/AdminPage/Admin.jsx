1057aaecc7bd43f56cf2aa36fb480e634ae272e2
Brianosumba <144438563+Brianosumba@users.noreply.github.com>
2024-10-11 14:33:03 +0200

    admin fix

--- frontend/src/pages/AdminPage/Admin.jsx
+++ frontend/src/pages/AdminPage/Admin.jsx
@@ -1,7 +1,6 @@
 import NavBar from "../../components/NavBar";
 import Footer from "../../components/Footer";
 import AdminCard from "../../components/Admincard";
-
 import { FaBell } from "react-icons/fa";
 import { useState } from "react";
 
