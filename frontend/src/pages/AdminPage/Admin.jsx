fa1b9433f260345301a5e0766108d8e45b09b7bd
Brianosumba <144438563+Brianosumba@users.noreply.github.com>
2024-10-23 14:54:10 +0200

    fix

--- frontend/src/pages/AdminPage/Admin.jsx
+++ frontend/src/pages/AdminPage/Admin.jsx
@@ -1,6 +1,6 @@
 import NavBar from "../../components/NavBar";
 import Footer from "../../components/footer";
-import AdminCard from "../../components/Admincard";
+import AdminCard from "../../components/AdminCard";
 import { FaBell } from "react-icons/fa";
 import { useState } from "react";
 import useAuthStore from "../Store/store";
