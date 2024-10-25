24ce10001a55a6bf9e2d35f319bd0b9545d4e6e7
Brianosumba <144438563+Brianosumba@users.noreply.github.com>
2024-10-25 13:41:07 +0200

    fix

--- frontend/src/components/ExcerciseCard.jsx
+++ frontend/src/components/ExcerciseCard.jsx
@@ -1,36 +1,33 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-export const ExerciseCard = ({ id, gifUrl, name, href, target }) => {
-  const ulrEncodedName = encodeURIComponent(name);
+
+export const ExerciseCard = ({ id, gifUrl, name, target }) => {
   return (
-    <Link to={href ?? `/exercise/${ulrEncodedName}`}>
-      <div
-        key={id} // Ensure each exercise has a unique 'id'
-        className="bg-white p-6 rounded-lg shadow-lg transition-transform duration-300 hover:scale-105 text-black cursor-pointer"
-      >
+    <Link to={`/exercise-detail/${id}`}>
+      <div className="bg-white p-6 rounded-lg shadow-lg transition-transform duration-300 hover:scale-105 text-black cursor-pointer">
         <img
           src={gifUrl}
           alt={name}
-          className="w-full h-60 object-cover rounded-md" // Increased height to 60 (from 40)
+          className="w-full h-60 object-cover rounded-md"
         />
         <div className="flex flex-col gap-2">
-          <p className="text-center mt-4 text-lg font-semibold capitalize ">
+          <p className="text-center mt-4 text-lg font-semibold capitalize">
             {name}
           </p>
-          {target ? (
-            <p className="text-center mt-4 text-lg font-semibold capitalize ">
+          {target && (
+            <p className="text-center text-lg font-semibold capitalize">
               {target}
             </p>
-          ) : null}
+          )}
         </div>
       </div>
     </Link>
   );
 };
+
 ExerciseCard.propTypes = {
   id: PropTypes.string.isRequired,
   gifUrl: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  href: PropTypes.string,
   target: PropTypes.string,
 };
