b256257a30dec012de953213c5af84f07520f17d
Brian <westbrayo@gmail.com>
2025-02-23 16:05:37 +0100

    pwa fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
+import manifest from "./public/manifest.json";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest,
+      workbox: {
+        maximumFileSizeToCacheInBytes: 6 * 1024 * 1024,
+        globPatterns: ["**/*.{js,css,html,ico,svg,png,jpg,jpeg,webp}"],
+        runtimeCaching: [
+          {
+            urlPattern: /.*\.(png|jpg|jpeg|svg|gif|webp)$/,
+            handler: "CacheFirst",
+            options: {
+              cacheName: "image-cache",
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60 * 24 * 7, // Cache images for 7 days
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
+});
