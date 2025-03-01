f2e8d0215df14a1790d1e5713d3d47954657871a
Brian <westbrayo@gmail.com>
2025-03-01 17:08:02 +0100

    fix

--- frontend/src/pages/Loginpage/Register.jsx
+++ frontend/src/pages/Loginpage/Register.jsx
@@ -4,7 +4,6 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { useNavigate } from "react-router-dom";
 import { register as registerUser } from "../../services/authService";
-import useAuthStore from "../Store/store";
 
 // Validation schema for form input
 const schema = yup.object().shape({
